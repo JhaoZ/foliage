39d7593c6b31e1faab22ef11dbad4fc18ed2c771
firecrest00 <a1021152875@gmail.com>
2024-02-24 18:45:34 -0600

    Made node elements draggable.

--- script.js
+++ script.js
@@ -60,6 +60,31 @@ function buildTree(data) { // data as a parameter, passing in a map
             }))
             .force("charge", d3.forceManyBody().strength(-500))
             .force("center", d3.forceCenter(width / 2, height / 2));
+
+        var drag = simulation => {
+
+            function dragstarted(event, d) {
+                if (!event.active) simulation.alphaTarget(0.3).restart();
+                d.fx = d.x;
+                d.fy = d.y;
+            }
+            
+            function dragged(event, d) {
+                d.fx = event.x;
+                d.fy = event.y;
+            }
+            
+            function dragended(event, d) {
+                if (!event.active) simulation.alphaTarget(0);
+                d.fx = null;
+                d.fy = null;
+            }
+            
+            return d3.drag()
+                .on("start", dragstarted)
+                .on("drag", dragged)
+                .on("end", dragended);
+        }
     
         // Render links
         svg.selectAll("line")
@@ -72,6 +97,7 @@ function buildTree(data) { // data as a parameter, passing in a map
             .data(nodes)
             .enter().append("div")
             .attr("class", "node").attr("id", d => d.data.id).attr("onclick", "clicknode(this)").attr("onmouseover", "hovernode(this)")
+            .call(drag(simulation))
             .html(d => String(d.data.id).substring(0,7));
     
     
